01a5892fb31ede1f620e47b3fb13e9755eb2e20b
Josh McCullough <josh.mccullough@gmail.com>
2016-09-12 21:57:58 -0400

    removes dist/cookie.d.ts and merges into dist/index.d.ts, meanwhile adding module wrapper

--- dist/es2015/index.d.ts
+++ dist/es2015/index.d.ts
@@ -1,3 +1,21 @@
-import { Cookie } from './cookie';
-export declare function configure(aurelia: any): void;
-export { Cookie };
+declare module 'aurelia-cookie' {
+    export function configure(aurelia: any): void;
+
+    export interface OptionsInterface {
+        expires?: Date;
+        expiry?: number;
+        path?: string;
+        domain?: string;
+        secure?: boolean;
+    }
+
+    export class Cookie {
+        static get(name: any): any;
+        static set(name: any, value: any, options: OptionsInterface): void;
+        static delete(name: any, domain?: any): void;
+        static all(): {};
+        static parse(str: any): {};
+        static encode(value: any): string;
+        static decode(value: any): string;
+    }
+}
